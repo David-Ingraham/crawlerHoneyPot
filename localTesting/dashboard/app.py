e5778eb1e89215c1569f3c405f198f17feaeda1d
David Ingraham <davidingrahamf@gmail.com>
2025-12-30 16:54:24 -0500

    turned off debug

--- localTesting/dashboard/app.py
+++ localTesting/dashboard/app.py
@@ -295,4 +295,4 @@ def get_malicious_activity():
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 8080))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
